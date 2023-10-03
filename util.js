fd149efe7a5dc4c098bf99b0f0a12b8e75deaf52
Garrick Henne <garrickhenne@outlook.com>
2023-10-02 17:39:25 -0700

    Implement generateRandomString

--- util.js
+++ util.js
@@ -1,5 +1,8 @@
+// return a randomly generated string containing numbers or letters.
 const generateRandomString = () => {
-  return 'a4mt2f';
+  // Math.random generates a random decimal, toString with radix converts to base 36 (allows us to have numbers and letters),
+  // take substring after decimal point.
+  return Math.random().toString(36).substring(2, 8);
 };
 
 module.exports = {
